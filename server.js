bb22cade4bd635a0df1536ab9f3f0126a626732b
kk77777 <kaushikgattani@gmail.com>
2020-04-05 23:31:09 +0530

    started creating routes

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ app.get('/', (req, res) => {
   });
 });
 
+app.use('/api/users', require('./routes/users'));
+app.use('/api/contacts', require('./routes/contact'));
+app.use('/api/auth', require('./routes/auth'));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
