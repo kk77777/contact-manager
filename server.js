ba9f8f11c9bb3deddaf2f9750434de3db0bf71eb
kk77777 <kaushikgattani@gmail.com>
2020-04-05 23:50:31 +0530

    setup routes

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/users', require('./routes/users'));
-app.use('/api/contacts', require('./routes/contact'));
+app.use('/api/contacts', require('./routes/contacts'));
 app.use('/api/auth', require('./routes/auth'));
 
 const PORT = process.env.PORT || 5000;
