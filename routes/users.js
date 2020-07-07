7a19ed77be72d9834b3074b08bc5e04e6b492668
kk77777 <kaushikgattani@gmail.com>
2020-07-07 14:59:29 +0530

    generating jwt

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const config = require('config');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
 
@@ -41,7 +43,24 @@ router.post(
 
       user.password = await bcrypt.hash(password, salt);
       await user.save();
-      res.send('User saved');
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtPrivateKey'),
+        {
+          expiresIn: 360000,
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json(token);
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send('Server error');
