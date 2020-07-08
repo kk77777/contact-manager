a5c60c7bb82957c342a031a476ad5a0ece7ac829
kk77777 <kaushikgattani@gmail.com>
2020-07-08 20:47:47 +0530

    put route for updating contact

--- routes/contacts.js
+++ routes/contacts.js
@@ -55,8 +55,34 @@ router.post(
 //@route PUT api/contacts/:id
 //@desc Update contact
 //@access Private
-router.put('/:id', (req, res) => {
-  res.send('Update Contact');
+router.put('/:id', auth, async (req, res) => {
+  const { name, email, phone, type } = req.body;
+
+  const contactFields = {};
+  if (name) contactFields.name = name;
+  if (email) contactFields.email = email;
+  if (phone) contactFields.phone = phone;
+  if (type) contactFields.type = type;
+
+  try {
+    let contact = await Contact.findById(req.params.id);
+    if (!contact) {
+      return res.status(404).json({ msg: 'Contact not found' });
+    }
+    if (contact.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'Not authorized' });
+    }
+    contact = await Contact.findByIdAndUpdate(
+      req.params.id,
+      { $set: contactFields },
+      { new: true }
+    );
+
+    res.json(contact);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send('Server Error');
+  }
 });
 
 //@route DELETE api/contacts/:id
